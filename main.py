1290bd98c5714d199399c10a18f982a0cf90501c
tailsbear <luky88@naver.com>
2021-04-12 20:53:30 +0900

    창 뛰으기 구연

--- main.py
+++ main.py
@@ -1,4 +1,22 @@
-a = 100
-print("a=",a)
-b = 10
-print(dir(b))
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget
+
+
+class MyApp(QWidget):
+
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+
+    def initUI(self):
+        self.setWindowTitle('My First Application')
+        self.move(300, 300)
+        self.resize(400, 200)
+        self.show()
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = MyApp()
+    sys.exit(app.exec_())
+    
