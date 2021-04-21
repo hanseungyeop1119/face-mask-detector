4123b9f71a6ecb4748626e5a68a0790057dd4176
tailsbear <luky88@naver.com>
2021-04-21 20:42:18 +0900

    data

--- Imgae.py
+++ Imgae.py
@@ -1,5 +1,6 @@
 from urllib.request import Request, urlopen
 import json
+import os
 
 api_url = 'https://api.github.com/repos/prajnasb/observations/contents/experiements/data/without_mask?ref=master'
 
@@ -14,6 +15,20 @@ contents = json.loads(directory_str)
 
 for i in range(len(contents)):
     content = contents[i]
-    print(content['download_url'])
+    #print(content['download_url'])
+    request = Request(content['download_url'])
+    response = urlopen(request)
+    image_data=response.read()
+
+    if not os.path.exists('data'):
+        os.mkdir('data')
+    if not os.path.exists('data/without_mask'):
+        os.mkdir('data/without_mask')
+
+    image_file =  open('data/without_mask/' + content['name'], 'wb')
+    image_file.write(image_data)
+    print('다운로드 완료(' + str(i + 1) + '/' +  str(len(contents)) + '): '+  content['name'])
+    # 다운로드 완료(10/500): 10.jpg
     break
+
 
